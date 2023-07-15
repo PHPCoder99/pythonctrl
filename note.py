3b616a2a73bb3dc61e4d1db7bcad9e7eb1e3321a
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-07-15 16:02:40 +0700

    file functions

--- note.py
+++ note.py
@@ -12,5 +12,15 @@ class Note:
         self.date = datetime.datetime.now()
         self.due_date = due_date
 
+    def to_csv_fmrt(self):
+        return f"{self.title},{self.desc},{datetime.date.strftime(self.due_date, '%Y/%m/%d')}\n"
+
+    def to_json_fmrt(self):
+        return "{" + f'"title": "{self.title}", "desc": "{self.desc}", "due_date": "{datetime.date.strftime(self.due_date, "%Y/%m/%d")}"' + "}"
+
+    @staticmethod
+    def parse_csv(arr):
+        return Note(arr[0], arr[1], datetime.datetime.strptime(arr[2], "%Y/%m/%d"))
+
     def __str__(self):
         return f"Записка {self.id} с именем {self.title} и описанием {self.desc}, в последний раз изменина в {datetime.date.strftime(self.date, '%Y/%m/%d')}, которую нужно выполнить к {datetime.date.strftime(self.due_date, '%Y/%m/%d')}"
